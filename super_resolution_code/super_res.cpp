24bc7a676eb9ce65ab4bd7b83a7dd1ca59043212
labolas <laboureur.nicolas@gmail.com>
2017-12-04 15:41:48 +0100

    bjbh

--- super_resolution_code/super_res.cpp
+++ super_resolution_code/super_res.cpp
@@ -231,7 +231,7 @@ vpRGBa bilinearInterpol(const vpImage<vpRGBa> &I,
  * @param N: facteur d agrandissement
  */
 static void
-upscale(const vpImage<vpRGBa> &LR, vpImage<vpRGBa> &HR, const unsigned int &N)
+upscale_bilinearInterpol(const vpImage<vpRGBa> &LR, vpImage<vpRGBa> &HR, const unsigned int &N)
 {
   int h=LR.getHeight(), w= LR.getWidth();
 
@@ -273,7 +273,7 @@ createDico(const vpImage<unsigned char> &comp, vector<unsigned char> * Dl, vecto
 static void
 Reconstruction(vpImage<vpRGBa> &LR, vpImage<vpRGBa> &HR)
 {
-	upscale(LR, HR, const unsigned int &N); // HR est l'image agrandi BF (bicubique ou lineaire interpol)
+	upscale_bilinearInterpol(LR, HR, 2); // HR est l'image agrandi BF (bicubique ou lineaire interpol)
 	//On vgg16 le resultat de ça
 	//On obtient des cartes de features
 	//On sélectionne un patch dans l'image et donc aussi dans les cartes de features
@@ -295,14 +295,15 @@ int main()
   vpImage<unsigned char> Cb_HR(h*n,w*n);
   vpImage<unsigned char> Cr_HR(h*n,w*n);
 
-  <unsigned char> Y_LR (h,w);
+  vpImage<unsigned char> Y_LR (h,w);
   vpImage<unsigned char> Cb_LR(h,w);
   vpImage<unsigned char> Cr_LR(h,w);
 
   vpImageIo::read(I_LR,"../data/img/lion.jpg") ;
 
   bicubicresize(I_LR, I_HR);
-
+  //upscale(I_LR, I_HR, n);
+  
   // convertion to YUV
   RGBtoYUV(I_LR, Y_LR, Cb_LR, Cr_LR);
 
